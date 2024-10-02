8e08938b2fba45ac4538463bc0410683e457175f
Gergir <aloneviahome@gmail.com>
2024-10-02 22:44:51 +0200

    add User, Target, Measurement models

---
+++ models/measurement.py
@@ -0,0 +1,19 @@
+from datetime import datetime, date
+
+from sqlalchemy import func, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from services.db_service import Base
+from models.target import Target
+
+class Measurement(Base):
+    __tablename__ = 'measurements'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    target_id: Mapped[int] = mapped_column(ForeignKey('targets.id'))
+    weight: Mapped[float] = mapped_column()
+    date: Mapped[date] = mapped_column()
+
+    created_at: Mapped[datetime] = mapped_column(insert_default=func.current_timestamp(), default=None) # handled by DB
+    updated_at: Mapped[datetime] = mapped_column(update_default=func.current_timestamp(), default=None)
+
+    target: Mapped["Target"] = relationship(back_populates="measurements")
---
+++ models/user.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from services.db_service import Base
+from models.target import Target
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
+    height: Mapped[float] = mapped_column()
+    weight: Mapped[float] = mapped_column()
+
+    created_at: Mapped[datetime] = mapped_column(insert_default=func.current_timestamp(), default=None)  # handled by DB
+    updated_at: Mapped[datetime] = mapped_column(update_default=func.current_timestamp(), default=None)
+
+    targets: Mapped[list["Target"]] = relationship(back_populates="user")
