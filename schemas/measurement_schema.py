71995ec45c1534e2e5a74aaada12a8584cb65678
Gergir <aloneviahome@gmail.com>
2024-10-03 21:56:03 +0200

    add user, Target, Measurement schemas

---
+++ schemas/measurement_schema.py
@@ -0,0 +1,19 @@
+from datetime import date
+from pydantic import BaseModel
+
+
+class MeasurementRequest(BaseModel):
+    target_id: int
+    weight: float
+    date: date
+
+
+class MeasurementResponse(BaseModel):
+    id: int
+    target_id: int
+    weight: float
+    date: date
+
+
+    class Config:
+        orm_mode = True
