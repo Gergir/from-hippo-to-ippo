9b154d41d2e177d8a805b7dd93602fbd887a6600
Gergir <aloneviahome@gmail.com>
2024-10-04 23:33:51 +0200

    connect user endpoint to database

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .target import Target
+from .measurement import Measurement
+
+
+__all__ = ['User', 'Target', 'Measurement']
--- routers/user.py
+++ routers/user.py
@@ -1,29 +1,63 @@
-from sys import prefix
-
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from services.db_service import get_db
+from models import User
+from schemas import UserRequest, UserResponse
 
 router = APIRouter(tags=["user"], prefix="/user")
 
-@router.get("/")
-async def get_all_users():
-    return {"message": "All found users"}
 
-@router.get("/{user_id}")
-async def get_user(user_id: int):
-    return {"message": f"User with id {user_id} found"}
+@router.get("/", response_model=List[UserResponse])
+async def get_all_users(db: Session = Depends(get_db)):
+    db_users = db.query(User).all()
+    return db_users
 
-@router.get("/name/{username}")
-async def get_user_by_name(username: str):
-    return {"message": f"User with name {username} found"}
 
-@router.post("/create")
-async def create_user():
-    return {"message": "User created"}
+@router.get("/{user_id}", response_model=UserResponse)
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise
+    return db_user
 
-@router.patch("/update/{user_id}")
-async def update_user(user_id: int):
-    return {"message": f"User with id {user_id} updated"}
+
+@router.get("/name/{username}", response_model=UserResponse)
+async def get_user_by_name(username: str, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.username == username).first()
+    if not db_user:
+        raise
+    return db_user
+
+
+@router.post("/create", response_model=UserResponse)
+async def create_user(request: UserRequest, db: Session = Depends(get_db)):
+    db_new_user = User(**request.model_dump())
+    db.add(db_new_user)
+    db.commit()
+    return db_new_user
+
+
+@router.patch("/update/{user_id}", response_model=UserResponse)
+async def update_user(user_id: int, request: UserRequest, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise
+
+    new_data_for_db_user = request.model_dump(exclude_unset=True)
+    for key, value in new_data_for_db_user.items():
+        setattr(db_user, key, value)
+
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
 
 @router.delete("/delete/{user_id}")
-async def delete_user(user_id: int):
-    return {"message": f"User with id {user_id} deleted"}
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise
+    db.delete(db_user)
+    db.commit()
+    return {"message": f"User {user_id} deleted."}
