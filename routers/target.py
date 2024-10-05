bbf4972194bd2c108f0f8ab864f86a9a2576da1f
Gergir <aloneviahome@gmail.com>
2024-10-05 23:24:29 +0200

    connect target endpoint to database

--- routers/target.py
+++ routers/target.py
@@ -1,33 +1,66 @@
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from services.db_service import get_db
+from models import Target
+from schemas import TargetRequest, TargetResponse
 
 router = APIRouter(tags=["target"], prefix="/target")
 
 
-@router.get("/")
-async def get_all_targets():
-    return {"message": "All found targets: ..."}
+@router.get("/", response_model=List[TargetResponse])
+async def get_all_targets(db: Session = Depends(get_db)):
+    db_targets = db.query(Target).all()
+    return db_targets
 
 
-@router.get("/{target_id}")
-async def get_target(target_id: int):
-    return {"message": f"Target with id {target_id} found"}
+@router.get("/{target_id}", response_model=TargetResponse)
+async def get_target(target_id: int, db: Session = Depends(get_db)):
+    db_target = db.query(Target).filter(Target.id == target_id).first()
+    if not db_target:
+        raise
+
+    return db_target
 
 
-@router.get("/name/{target_name}")
-async def get_target_by_name(target_name: str):
-    return {"message": f"Target with name {target_name} found"}
+@router.get("/name/{target_name}", response_model=TargetResponse)
+async def get_target_by_name(target_name: str, db: Session = Depends(get_db)):
+    db_target = db.query(Target).filter(Target.title == target_name).first()
+    if not db_target:
+        raise
+
+    return db_target
 
 
-@router.post("/create")
-async def create_target():
-    return {"message": "Target has been created"}
+@router.post("/create", response_model=TargetResponse)
+async def create_target(request: TargetRequest, db: Session = Depends(get_db)):
+    db_new_target = Target(**request.model_dump())
+    db.add(db_new_target)
+    db.commit()
+    return db_new_target
 
 
-@router.patch("/update/{target_id}")
-async def update_target(target_id: int):
-    return {"message": f"Target with id {target_id} updated"}
+@router.patch("/update/{target_id}", response_model=TargetResponse)
+async def update_target(target_id: int, request: TargetRequest, db: Session = Depends(get_db)):
+    db_target = db.query(Target).filter(Target.id == target_id).first()
+    if not db_target:
+        raise
+
+    new_data_for_db_target = request.model_dump()
+    for key, value in new_data_for_db_target.items():
+        setattr(db_target, key, value)
+
+    db.commit()
+    db.refresh(db_target)
+    return db_target
 
 
 @router.delete("/delete/{target_id}")
-async def delete_target(target_id: int):
+async def delete_target(target_id: int, db: Session = Depends(get_db)):
+    db_target = db.query(Target).filter(Target.id == target_id).first()
+    if not db_target:
+        raise
+
+    db.delete(db_target)
+    db.commit()
     return {"message": f"Target with id {target_id} deleted"}
