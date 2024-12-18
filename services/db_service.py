9f9ef2ceeaa089fb27e25dc71bd7d8b7a51f6aaf
Gergir <aloneviahome@gmail.com>
2024-12-18 05:14:25 +0100

    add testing environment

--- services/db_service.py
+++ services/db_service.py
@@ -4,9 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+if os.getenv("ENV") == "TEST":
+    engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
